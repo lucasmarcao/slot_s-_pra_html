93e6bbe531eb635d746f46d4dbced3d093e7a520
Lucas Marcão <69424024+lucasmarcao@users.noreply.github.com>
2022-02-10 21:07:11 -0300

    matei

--- Crud-base/js/excluir.js
+++
@@ -1,4 +0,0 @@
-function excluirBotao() {
-  const textoExclui = document.getElementById('texto-exclusao');
-  textoExclui.innerHTML = "Deseja excluir o item de id  : <br> { "+entradaId.value+" } ?"
-}
